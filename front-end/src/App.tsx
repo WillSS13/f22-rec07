f7b6664b0d3333c2ba568ca5a032a6757c06fe66
Will Squibb <wsquibb@andrew.cmu.edu>
2023-10-10 14:39:48 -0400

    Task 2 --- Undo

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -48,6 +48,12 @@ class App extends React.Component<Props, GameState> {
     this.setState({ cells: json['cells'], currentPlayer: json['currentPlayer'], winner: json['winner']});
   }
 
+  undo = async () => {
+    const response = await fetch('/undo');
+    const json = await response.json();
+    this.setState({ cells: json['cells'], currentPlayer: json['currentPlayer'], winner: json['winner']});
+  }
+
   /**
    * play will generate an anonymous function that the component
    * can bind with.
@@ -121,7 +127,7 @@ class App extends React.Component<Props, GameState> {
         <div id="bottombar">
           <button onClick={/* get the function, not call the function */this.newGame}>New Game</button>
           {/* Exercise: implement Undo function */}
-          <button>Undo</button>
+          <button onClick={this.undo}>Undo</button>
         </div>
         <div id="instructions">
           {/* If winner is null, say which player's turn it is */}
